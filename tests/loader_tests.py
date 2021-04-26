91782d5c7b3b955dfc95ab3c589059328f668586
Konstantin Freidlin <freidlinks@ya.ru>
2021-04-26 23:48:16 +0300

    ✅ Update features tests

--- tests/loader_tests.py
+++ tests/loader_tests.py
@@ -48,7 +48,7 @@ def test_write_read_only_error():
 
 
 def test_write_to_nonexistent_directory():
-    """Check if raise error with writing to no nonexistent directory"""
+    """Check if raise error with writing to no nonexistent directory."""
 
     with requests_mock.Mocker() as mock_up:
         mock_up.get(DEMO_URL, text="demo")
@@ -56,3 +56,49 @@ def test_write_to_nonexistent_directory():
         with tempfile.TemporaryDirectory() as temp_dir:
             with pytest.raises(FileNotFoundError):
                 _ = download(DEMO_URL, f"{temp_dir}/not/existent/dir")
+
+
+def _get_all_files(path):
+    file_list = os.listdir(path)
+    ref_path = list(filter(lambda x: x.endswith("_files"), file_list))[0]
+    file_list += os.listdir(f"{path}/{ref_path}")
+    return sorted(file_list)
+
+
+def test_download_page():
+    """Check main features of app."""
+
+    with open("tests/fixtures/demo_page/in/example.html", "r") as file:
+        html_text = file.read()
+    with open("tests/fixtures/demo_page/in/css/styles.css", "rb") as file:
+        css_content = file.read()
+    with open("tests/fixtures/demo_page/in/img/googlelogo_color_272x92dp.png", "rb") as file:
+        img_content = file.read()
+    with open("tests/fixtures/demo_page/in/js/scripts.js", "rb") as file:
+        js_content = file.read()
+
+    with requests_mock.Mocker() as mock_up:
+        mock_up.get(DEMO_URL, text=html_text)
+        mock_up.get(f"{DEMO_URL}/css/styles.css", content=css_content)
+        mock_up.get(f"{DEMO_URL}/img/googlelogo_color_272x92dp.png", content=img_content)
+        mock_up.get(f"{DEMO_URL}/js/scripts.js", content=js_content)
+
+        with tempfile.TemporaryDirectory(dir="tests/fixtures/demo_page/") as temp_dir:
+            result = download(DEMO_URL, temp_dir)
+
+            _, filename = os.path.split(result)
+
+            with open(result, "r") as file:
+                result_html = file.read()
+            with open(f"tests/fixtures/demo_page/out/{filename}", "r") as file:
+                right_html = file.read()
+            assert result_html == right_html
+
+            refs_main_path = result.replace(".html", "_files")
+            for ref_path in os.listdir(refs_main_path):
+                _, ref_filename = os.path.split(ref_path)
+                with open(f"{refs_main_path}/{ref_path}", "rb") as file:
+                    result_ref = file.read()
+                with open(f"tests/fixtures/demo_page/out/example-ru_files/{ref_filename}", "rb") as file:
+                    right_ref = file.read()
+                assert result_ref == right_ref
