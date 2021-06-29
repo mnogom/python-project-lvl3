5f734851a019d98f152587c9065122ef96148fee
Konstantin Freidlin <freidlinks@ya.ru>
2021-06-30 02:23:03 +0300

    ♻️  optimization loader.generate_name function

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -12,43 +12,38 @@ from page_loader.file_system import make_dir, save_file
 from page_loader.network import make_request
 
 
-def _generate_name(url: str, isdir=False) -> str:
+def _generate_name(url: str, is_dir=False) -> str:
     """Convert url to local name.
 
     :param url: input url
-    :param isdir: is name for directory
+    :param is_dir: is name for directory
     :return: local name for url
     """
 
-    url_name, ext = os.path.splitext(url)
+    # TODO: #1.1 if downloaded page ends with '/' then extension will be html
+    if url.endswith("/"):
+        url_name, ext = url[:-1], None
+    else:
+        url_name, ext = os.path.splitext(url)
+    ext = ext if ext else ".html"
 
-    # TODO: #1.1 some trouble when root url ends with "/"
-    if url_name.endswith("/"):
-        url_name = url_name[:-1]
-
-    parsed_name_url = urlparse(url_name)
-    filename = "{}{}{}".format(
-        parsed_name_url.netloc,
-        parsed_name_url.path,
-        f"?{parsed_name_url.query}" if parsed_name_url.query else "")
+    scheme = f"{urlparse(url_name).scheme}://"
+    filename = url_name.replace(scheme, '', 1)
     filename = re.sub(r"\W", "-", filename)
 
-    if isdir:
+    if is_dir:
         full_filename = filename + "_files"
         logging.info(f"Create name '{full_filename}' for  directory of "
                      f"references of '{url}'")
         return full_filename
 
-    if ext:
-        full_filename = filename + ext
-    else:
-        full_filename = filename + ".html"
-        logging.info(f"Create name '{full_filename}' for url '{url}'")
+    full_filename = filename + ext
+    logging.info(f"Create name '{full_filename}' for url '{url}'")
 
     return full_filename
 
 
-def _is_local_asset(item_url: str, page_url: str) -> bool:
+def _is_local_asset(page_url: str, item_url: str) -> bool:
     """Check if asset is local.
 
     :param item_url: reference url
@@ -56,21 +51,13 @@ def _is_local_asset(item_url: str, page_url: str) -> bool:
     :return: True/False
     """
 
-    page_url_parsed = urlparse(page_url)
-    item_url_parsed = urlparse(item_url)
-
     if not item_url:
-        logging.info("Item hasn't references in attribute")
         return False
-    if item_url.startswith("data:"):
-        logging.info("Type of item reference is 'Data URL'")
-        return False
-    if (not item_url_parsed.netloc
-            or item_url_parsed.netloc == page_url_parsed.netloc):  # noqa: W503
-        logging.info(f"Item reference '{item_url}' is local")
-        return True
-    logging.info(f"Item reference '{item_url}' is not local")
-    return False
+
+    page_url_netloc = urlparse(page_url).netloc
+    item_url_netloc = urlparse(urljoin(page_url, item_url)).netloc
+
+    return page_url_netloc == item_url_netloc
 
 
 def _switch_assets(soup, page_url: str, asset_rel_dir: str) -> list:
@@ -99,7 +86,7 @@ def _switch_assets(soup, page_url: str, asset_rel_dir: str) -> list:
 
             item_url = item.get(attr)
 
-            if _is_local_asset(item_url, page_url):
+            if _is_local_asset(page_url, item_url):
                 item_url = urljoin(page_url, item_url)
                 filename = _generate_name(item_url)
 
@@ -146,17 +133,17 @@ def download(url: str, path=os.getcwd()) -> str:
     :return: path to downloaded page
     """
 
-    # TODO: #1.2 some trouble when root url ends without "/"
+    # TODO: #1.2 url must ends with '/'
     full_url = url if url.endswith("/") else url + "/"
 
     response = make_request(url)
 
     page_name = _generate_name(full_url)
-    asset_rel_dir = _generate_name(full_url, isdir=True)
+    asset_rel_dir = _generate_name(full_url, is_dir=True)
     asset_abs_dir = os.path.join(path, asset_rel_dir)
 
     soup = BeautifulSoup(response.text, features="html.parser")
-    assets_to_download = _switch_assets(soup, full_url, asset_rel_dir)
+    assets_to_download = _switch_assets(soup, url, asset_rel_dir)
 
     page_path = save_file(os.path.join(path, page_name),
                           "w",
--- tests/test_components.py
+++ tests/test_components.py
@@ -8,22 +8,28 @@ from page_loader.logger_agent import get_logger
 
 
 INPUT = ("https://ru.hexlet.io/",
-         # "https://ru.hexlet.io",
          "https://ru.hexlet.io/courses/intro_to_git/",
-         "https://ru.hexlet.io/my/ratings/month?q%5Bcity_id_eq%5D=1")
+         "https://ru.hexlet.io/my/ratings/month?q%5Bcity_id_eq%5D=1/")
 
-RESULT = ("ru-hexlet-io.html",
-          # "ru-hexlet-io.html",
-          "ru-hexlet-io-courses-intro_to_git.html",
-          "ru-hexlet-io-my-ratings-month-q-5Bcity_id_eq-5D-1.html")
+EXPECTED_PAGE_NAMES = ("ru-hexlet-io.html",
+                       "ru-hexlet-io-courses-intro_to_git.html",
+                       "ru-hexlet-io-my-ratings-month-q-5Bcity_id_eq-5D-1.html")
+
+EXPECTED_DIR_NAMES = ("ru-hexlet-io_files",
+                      "ru-hexlet-io-courses-intro_to_git_files",
+                      "ru-hexlet-io-my-ratings-month-q-5Bcity_id_eq-5D-1_files")
 
 
-@pytest.mark.parametrize("url, result",
-                         [
-                             (a, b) for a, b in zip(INPUT, RESULT)
-                         ])
-def test_local_name(url, result):
-    assert _generate_name(url) == result
+@pytest.mark.parametrize("url, is_dir, result", [
+    (INPUT[0], False, EXPECTED_PAGE_NAMES[0]),
+    (INPUT[1], False, EXPECTED_PAGE_NAMES[1]),
+    (INPUT[2], False, EXPECTED_PAGE_NAMES[2]),
+    (INPUT[0], True, EXPECTED_DIR_NAMES[0]),
+    (INPUT[1], True, EXPECTED_DIR_NAMES[1]),
+    (INPUT[2], True, EXPECTED_DIR_NAMES[2]),
+])
+def test_local_name(url, is_dir, result):
+    assert _generate_name(url, is_dir) == result
 
 
 @pytest.mark.parametrize("debug_mode, right_log_level",
